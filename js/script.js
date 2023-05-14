bb77fb56dd679b631868d06c6946bd7739277b26
David Forrester <david.o.forrester@gmail.com>
2023-05-15 09:59:56 +1000

    created board and added to the dom

--- js/script.js
+++ js/script.js
@@ -1,8 +1,47 @@
-// Variables
+// module
+const Gameboard = (() => {
+  board = [];
+  for (let i = 0; i < 3; i++) {
+    this.board.push([]);
+    for (let j = 0; j < 3; j++) {
+      this.board[i].push("");
+    }
+  }
 
+  const display = () => {
+    const divs = [];
 
-// Code body
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        const div = document.createElement("div");
+        div.innerText = board[i][j];
+        div.className = "square"
+        divs.push(div);
+      }
+    }
 
+    const container = document.querySelector(".gameboard");
 
-// Functions
+    for (let div of divs) {
+      container.appendChild(div);
+    }
+  };
 
+  return { board, display };
+})();
+
+// factory
+//Player()
+
+// module
+//PlayRound()
+
+// Global Code
+board = Gameboard.board;
+
+board[0][0] = "X";
+board[1][2] = "O";
+board[2][1] = "X";
+
+console.log(board);
+Gameboard.display();
